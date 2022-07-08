af5364c0990ee5fac005afa294cc9883ced12bc1
Suleymanov Andrey <sasus37@gmail.com>
2022-07-08 22:04:42 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include <vector>
+const int Size = 4;
+void swapvec ( std::vector <int> &a, int b[]){
+    if (a.size() != Size) {std::cout << "vector size dont equal array size!";}
+    else {
+        for (int i = 0; i < Size; i++){
+            int temp = a [i];
+            a[i]= b[i];
+            b[i] = temp;
+        }
+    }
+}
 int main() {
     std::vector<int> a = {1,2,3,4};
 
-    int b[] = {2,4,6,8};
+    int b[Size] = {2,4,6,8};
 
     swapvec(a,b);
 
